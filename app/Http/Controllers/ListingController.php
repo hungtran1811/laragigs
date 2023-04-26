16bb07d53d0c1a0889252356e4abb69254e75a3b
HungTran <hungtran00.nt@gmail.com>
2023-04-26 12:32:29 +0700

    FlashMess, KeepOldDataInForm, Pagination, UploadFile

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
 {
     public static function index(){
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag','search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag','search']))->paginate(6)
         ]);
     }
 
@@ -35,7 +35,11 @@ class ListingController extends Controller
             'description' => 'required'
         ]);
 
+        if($request->hasFile('logo')){
+            $formFields['logo'] = $request->file('logo')->store('logos','public');
+        }
+
         Listing::create($formFields);
-        return redirect('/');
+        return redirect('/')->with('message', 'Listing created successfully');
     }
 }
